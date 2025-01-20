1a68a71552f1464e99a78b1c9e4ab0e576b7285f
yash@8123 <116726926+yash08123@users.noreply.github.com>
2025-01-21 00:49:45 +0530

    Set DJANGO_SETTINGS_MODULE in build script and reorder imports in models and routing

--- chat_app/buildtwo.sh
+++ chat_app/buildtwo.sh
@@ -1,2 +1,3 @@
 cd chat_app
+export DJANGO_SETTINGS_MODULE=chat_app.settings
 daphne -b 0.0.0.0 -p 8000 chat_app.asgi:application
--- chat_app/chat/models.py
+++ chat_app/chat/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+
 
 class Message(models.Model):
     sender = models.ForeignKey(User, related_name="sent_messages", on_delete=models.CASCADE)
