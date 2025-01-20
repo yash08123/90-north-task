135324e073bf0b9946523744c31166b838c53cb4
yash@8123 <116726926+yash08123@users.noreply.github.com>
2025-01-20 15:37:50 +0530

    Fix import path for routing in ASGI configuration

--- chat_app/chat_app/asgi.py
+++ chat_app/chat_app/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing as routing
+import chat_app.chat.routing as routing
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
