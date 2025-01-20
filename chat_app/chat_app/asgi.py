1c3318d031dd536e50b8611f6be80be68f28e104
yash@8123 <116726926+yash08123@users.noreply.github.com>
2025-01-20 15:33:48 +0530

    asgi fix

--- chat_app/chat_app/asgi.py
+++ chat_app/chat_app/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat import routing
+import chat.routing as routing
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
